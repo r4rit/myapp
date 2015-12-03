1c0ab048f3f04c5e3178bf8b2509530f4ccf89be
r4rit <lucidlilac@gmail.com>
2015-12-03 18:51:07 +0900

    동적페이지

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var data={count:0};
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  data.count++;
+  res.render('DB_ejs', data);
+});
+
+app.get('/reset', function (req, res) {
+  data.count=0;
+  res.render('DB_ejs', data);
+});
+
+
+app.get('/set/count', function (req, res) {
+  if(req.query.count) data.count=req.query.count;
+  res.render('DB_ejs', data);
+});
+
+app.get('/set/:num', function (req, res) {
+  data.count=req.params.num;
+  res.render('DB_ejs', data);
 });
 
 app.listen(3000, function() {
