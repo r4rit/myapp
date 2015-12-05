bf64d64de5421b7242c45e24c3260c2177c80981
r4rit <lucidlilac@gmail.com>
2015-12-06 01:45:22 +0900

    회원가입 다이렉트

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var path = require('path');
+var mysql = require('mysql');
+
 var app = express();
+var connection = mysql.createConnection({
+  host : 'localhost',
+  port : 3306,
+  user : '',
+  password : '',
+  database : ''
+});
+
 
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+app.get('/main_guest', function (req, res) {
   res.render('DB_main_guest');
 });
 
-app.get('/resistration', function (req, res) {
-  res.render('DB_resistration');
+app.get('/resist', function (req, res) {
+  res.render('DB_resist');
 });
+
+app.get('/resist_agreement', function(req, res) {
+  res.render('DB_resist_agreement')
+})
 
 app.get('/main_admin', function (req, res) {
   res.render('DB_main_admin');
